6850653841cc813e0d1e50c7e0f54ba19159fcff
byLLIPyT <byllipyt@icloud.com>
2021-06-03 08:31:06 +0300

    Initial Commit

---
+++ RightOnTable/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RightOnTable
+//
+//  Created by Александр Уткин on 03.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
